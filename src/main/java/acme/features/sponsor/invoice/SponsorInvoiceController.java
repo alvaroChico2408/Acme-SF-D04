5b27a4813524e49e76e397a658e88b4e60f01da0
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-04-10 12:28:40 +0200

    [feat]: List Invoices of a sponsorship and show Invoices details

---
+++ src/main/java/acme/features/sponsor/invoice/SponsorInvoiceController.java
@@ -0,0 +1,33 @@
+
+package acme.features.sponsor.invoice;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.entities.invoice.Invoice;
+import acme.roles.Sponsor;
+
+@Controller
+public class SponsorInvoiceController extends AbstractController<Sponsor, Invoice> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private SponsorInvoiceListService	listService;
+
+	@Autowired
+	private SponsorInvoiceShowService	showService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
+	}
+
+}
