fbcf65307c1290d5cf45aa61327c7e63be05fddc
Enrgaraba <enrgaraba@alum.us.es>
2024-04-25 12:58:04 +0200

    [feat] Convertirse en client

---
+++ src/main/java/acme/features/authenticated/client/AuthenticatedClientRepository.java
@@ -0,0 +1,20 @@
+
+package acme.features.authenticated.client;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.data.accounts.UserAccount;
+import acme.client.repositories.AbstractRepository;
+import acme.roles.Manager;
+
+@Repository
+public interface AuthenticatedClientRepository extends AbstractRepository {
+
+	@Query("select ua from UserAccount ua where ua.id = :userAccountId")
+	UserAccount findOneUserAccountById(int userAccountId);
+
+	@Query("select c from Client c where c.userAccount.id = :userAccountId")
+	Manager findOneClientByUserAccountId(int userAccountId);
+
+}
