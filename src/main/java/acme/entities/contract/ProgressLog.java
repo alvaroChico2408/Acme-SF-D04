6c7dae01338a9832c74b2cd824b4b3a23a29c45a
Enrgaraba <enrgaraba@alum.us.es>
2024-07-06 20:12:42 +0200

    [fix] Test completos (Faltan docs de test y algunos analysis report)

--- src/main/java/acme/entities/contract/ProgressLog.java
+++ src/main/java/acme/entities/contract/ProgressLog.java
@@ -5,9 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -28,9 +26,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(indexes = {
-	@Index(columnList = "contract_id")
-})
+/*
+ * @Table(indexes = {
+ * 
+ * @Index(columnList = "contract_id")
+ * })
+ */
 public class ProgressLog extends AbstractEntity {
 	// Serialisation identifier -----------------------------------------------
 
